15ee77f5c516376b65051899cb7d6d8600db2326
Rafael Perez <rafaelandresph@gmail.com>
2022-06-16 13:36:43 -0400

    Add Drexel and jefferson U logo and more information.

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,29 +1,4 @@
 import React from 'react';
-// import { Link } from "react-router-dom";
-
-// const Navbar = () => {
-//     return (
-//         <div>
-//             <li>
-//                 <Link to="/">Home</Link>
-//             </li>
-//             <li>
-//                 <Link to="/Projects">Projects</Link>
-//             </li>
-//             <li>
-//                 <Link to="/Photography">Photography</Link>
-//             </li>
-//             <li>
-//                 <Link to="/Illustration">Illustration</Link>
-//             </li>
-//             <li>
-//                 <Link to="/ReadingList">Reading List</Link>
-//             </li>
-//         </div>
-//     );
-// }
-
-// export default Navbar;
 import {Container, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 class Topbar extends React.Component {
@@ -54,6 +29,15 @@ class Topbar extends React.Component {
                     <Nav className="ml-auto" navbar>
                         <NavItem>
                             <NavLink href="/photography/">Photography</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/illustration/">Illustrations</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/projects/">Projects</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/ReadingList/">Reading List</NavLink>
                         </NavItem>
                     </Nav>
                 </Collapse>
