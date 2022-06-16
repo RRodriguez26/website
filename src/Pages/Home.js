1bb850297ba19a756585040b783c53bb66d9d39c
Rafael Perez <rafaelandresph@gmail.com>
2022-06-16 15:24:50 -0400

    Add rendering into profile and functions to switch toggling.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+import Profile from "../components/Profile";
 const Home = () => {
     return(
     <div>
+        <Profile/>
         <h3>Welcome to the Perazy experience!</h3>
     </div>
     );
---
+++ src/components/Education.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Container, Row, Col, Media} from 'reactstrap';
+import moment from 'moment';
+import profile from '../information/profile.json';
+
+class Education extends React.Component {    
+    render() {
+        return <Container>
+            <Row>
+                <Col>
+                {profile.education.map(function (edu, i) {
+                    return <Media key={i}>
+                        <Media left top href={edu.url}>
+                            <Media object src={edu.logo} alt={edu.institute}/>
+                        </Media>
+                        <Media body>
+                            <Media heading>
+                                <a href={edu.url}>{edu.institute}</a>
+                            </Media>
+                            {
+                                [
+                                    {
+                                        "key": "Title",
+                                        "value": edu.title,
+                                    },
+                                    {
+                                        "key": "Graduation Year",
+                                        "value": edu.graduationYear
+                                      },
+                                      {
+                                        "key": "Duration",
+                                        "value": edu.durationInYears + " year(s)"
+                                      }
+                                ].map(function (object) {
+                                    return <div>
+                                            <Row>
+                                                <Col className="formLabel">{object.key}:</Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>{object.value}</Col>
+                                            </Row>
+                                            </div>
+                                })
+                            }
+                        </Media>
+                    </Media>
+                })}
+                </Col>
+            </Row>
+        </Container>
+    }
+}
+
+export default Education;
