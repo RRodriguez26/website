d7c2dddac67a7016e4c3c5d6898d02caba8520c9
Rafael Perez <rafaelandresph@gmail.com>
2022-06-22 01:53:52 -0400

    Comment out errors due to undefined object

--- src/components/PhilosophyBooks.js
+++ src/components/PhilosophyBooks.js
@@ -2,48 +2,58 @@ import React from 'react';
 import {Container, Row, Col, Media} from 'reactstrap';
 import reading from '../information/reading-list.json';
 
-// TODO: Fix the body to display a list of books based on categories
+// TODO: Fix the body to display a list of books based on categories, in this case it's Philosophy [ ]
 
 class PhilosophyBooks extends React.Component {    
     render() {
         return <Container>
             <Row>
                 <Col>
-                {reading.genre.map(function (book, i) {
-                    return <Media key={i}>
-                        <Media body>
-                            {
-                                [
-                                    {
-                                        "key": "Title",
-                                        "value": book.title,
-                                    },
-                                    {
-                                        "key": "Author",
-                                        "value": book.author,
-                                      },
-                                      {
-                                        "key": "Year",
-                                        "value": book.year,
-                                      },
-                                      {
-                                        "key": "Publisher",
-                                        "value": book.publisher,
-                                      }
-                                ].map(function (object) {
-                                    return <div>
-                                            <Row>
-                                                <Col className="formLabel">{object.key}:</Col>
-                                            </Row>
-                                            <Row>
-                                                <Col>{object.value}</Col>
-                                            </Row>
-                                            </div>
-                                })
-                            }
-                        </Media>
-                    </Media>
-                })}
+                    {reading.genre.map(function (books, i) {
+                        return (
+                            <div key={i}>
+                                <Media>
+                                    <Media body>
+                                        {/* {books.Philosophy.map(function (book, j){
+                                                return (
+                                                <div key={j}>
+                                                    {
+                                                    [
+                                                        {
+                                                            "key": "Title",
+                                                            "value": book.Title,
+                                                        },
+                                                        {
+                                                            "key": "Author",
+                                                            "value": book.Author,
+                                                          },
+                                                          {
+                                                            "key": "Year",
+                                                            "value": book.Year,
+                                                          },
+                                                          {
+                                                            "key": "Publisher",
+                                                            "value": book.Publisher,
+                                                          }
+                                                    ].map(function (object) {
+                                                        return <div>
+                                                                <Row>
+                                                                    <Col className="formLabel">{object.key}:</Col>
+                                                                </Row>
+                                                                <Row>
+                                                                    <Col>{object.value}</Col>
+                                                                </Row>
+                                                                </div>
+                                                    })
+                                                }
+                                                </div>
+                        );
+                    })   
+                                        } */}
+                                    </Media>
+                                </Media>
+                            </div>
+                            )})}
                 </Col>
             </Row>
         </Container>
