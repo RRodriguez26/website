92662e05b86e5314d4609569d2684694b0f4e0c5
Rafael Perez <rafaelandresph@gmail.com>
2022-06-27 23:11:43 -0400

    Change background color and insert image(not working)

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -19,25 +19,25 @@ class Topbar extends React.Component {
 
     render() {
         return (
-        <Navbar color="dark" dark expand="md">
+        <Navbar className="nav-color" dark="true" expand="sm">
             <Container>
-                <NavbarBrand href="/">
+                <NavbarBrand className="nav-text-color" href="/">
                     <span>Rafael Rodriguez</span>
                 </NavbarBrand>
                 <NavbarToggler onClick={this.toggle}/>
                 <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <NavLink href="/photography/">Photography</NavLink>
+                            <NavLink className="nav-text-color" href="/photography/">Photography</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="/illustration/">Illustrations</NavLink>
+                            <NavLink className="nav-text-color" href="/illustration/">Illustrations</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="/projects/">Projects</NavLink>
+                            <NavLink className="nav-text-color" href="/projects/">Projects</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="/readinglist/">Reading List</NavLink>
+                            <NavLink className="nav-text-color" href="/readinglist/">Reading List</NavLink>
                         </NavItem>
                     </Nav>
                 </Collapse>
