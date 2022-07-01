baa7b48bb08ed1bb8cf69f01ff8548aa8e2de959
Rafael Perez <rafaelandresph@gmail.com>
2022-07-01 02:01:26 -0400

    Add sections of blog entry in blog page

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Input, Container } from "reactstrap";
 const Blogs = () => {
     return (
     <div>
+
         <h3>Daily Blogs</h3>
-        <h1>Entry 1</h1>
+        <Container>
+            And this is for the inbox for many updates on game developmentm, studies, artistic process, and readings.
+            <h1>
+                Title: The Progress
+            </h1>
+            <p>
+                As I am typing this, it is July 1st 1:59 AM, Volume 2 of Stranger Things will come out, my first driving lesson will take place soon. And I will be working soon. I am learning to do programing (Web Development specifically) as you all are reading this one day. 
+            </p>
+        </Container>
+
     </div>
 
     );
