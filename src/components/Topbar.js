a4ffa0e204b918e905124bf5b0216cd102a9b0cf
Rafael Perez <rafaelandresph@gmail.com>
2023-03-24 21:28:11 -0400

    remove unnecessary functions and add flex to display

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import { Container, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Container, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 class Topbar extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
-
-    toggle() {
-        this.setState({ 
-            isOpen: !this.state.isOpen
-        });
-    }
-
     render() {
         return (
-        <Navbar className="nav-color" dark="true" expand="sm">
-            <Container>
+            <Navbar className="nav-color" dark="true" expand="sm">
+                <Container className="d-flex">
                 <NavbarBrand className="nav-text-color" href="/">
                     <span>Rafael Rodriguez</span>
                 </NavbarBrand>
                 <NavbarToggler onClick={this.toggle}/>
-                <Collapse isOpen={this.state.isOpen} navbar>
                     <Nav className="ml-auto" navbar>
                         <NavItem>
                             <NavLink className="nav-text-color" href="/photography/">Photography</NavLink>
@@ -43,7 +27,6 @@ class Topbar extends React.Component {
                             <NavLink className="nav-text-color" href="/blogs/">Blogs</NavLink>
                         </NavItem>
                     </Nav>
-                </Collapse>
             </Container>
         </Navbar>
         );
@@ -51,3 +34,52 @@ class Topbar extends React.Component {
 }
 
 export default Topbar;
+
+// import React from 'react';
+// import {
+//     Navbar,
+//     NavbarBrand,
+//     Nav,
+//     NavItem,
+//     NavLink,
+//     UncontrolledDropdown,
+//     DropdownToggle,
+//     DropdownMenu,
+//     DropdownItem,
+//     NavbarText,
+// } from 'reactstrap';
+
+// function Topbar(args) {
+
+//     return (
+//         <div>
+//             <Navbar {...args}>
+//                 <NavbarBrand href="/">reactstrap</NavbarBrand>
+//                     <Nav className="me-auto" navbar>
+//                         <NavItem>
+//                             <NavLink href="/components/">Components</NavLink>
+//                         </NavItem>
+//                         <NavItem>
+//                             <NavLink href="https://github.com/reactstrap/reactstrap">
+//                                 GitHub
+//                             </NavLink>
+//                         </NavItem>
+//                         <UncontrolledDropdown nav inNavbar>
+//                             <DropdownToggle nav caret>
+//                                 Options
+//                             </DropdownToggle>
+//                             <DropdownMenu right>
+//                                 <DropdownItem>Option 1</DropdownItem>
+//                                 <DropdownItem>Option 2</DropdownItem>
+//                                 <DropdownItem divider />
+//                                 <DropdownItem>Reset</DropdownItem>
+//                             </DropdownMenu>
+//                         </UncontrolledDropdown>
+//                     </Nav>
+//                     <NavbarText>Simple Text</NavbarText>
+//             </Navbar>
+//         </div>
+//     );
+// }
+
+// export default Topbar;
